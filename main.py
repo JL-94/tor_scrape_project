979e01f320efd1c4e86276ae8b3d2ebf7106d5c0
Jamie Lawson <jamielawson94@hotmail.com>
2022-06-26 22:36:27 +0100

    refactor

--- main.py
+++ main.py
@@ -3,19 +3,15 @@ import lxml
 import bs4
 from torpy.http.requests import tor_requests_session
 
-
-
 def get_ip():
     # Pulls your public IP address from https://www.ident.me.
 
     pub_ip = urllib.request.urlopen('https://v4.ident.me').read().decode('utf8')
     return pub_ip
 
-
-
 def tor_check():
-# Runs a http request through the TOR network, and returns the public IP address of the TOR exit node used. 
-# TOR network is slow and can be temperamental, be patient if it does not work first time. 
+    # Runs a http request through the TOR network, and returns the public IP address of the TOR exit node used. 
+    # TOR network is slow and can be temperamental, be patient if it does not work first time. 
 
     url = "https://v4.ident.me"
 
@@ -27,13 +23,15 @@ def tor_check():
     exit_node = tag[0].get_text()
     print('Using TOR exit node: ' + exit_node)
 
-    return data
+    return exit_node
 
 def main():
     pub_ip = get_ip()
     print('Your source public IP is: ' + pub_ip)
+    print('This will be hidden.')
     tor_check()
 
-main()
+if __name__ == "__main__":
+    main()
 
 
